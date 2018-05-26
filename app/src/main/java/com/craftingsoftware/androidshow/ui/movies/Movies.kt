1bc8cb8af3eec815c431aa7598c68a9c40231e60
Giusto <sipos.eduard1@gmail.com>
2018-05-26 14:38:18 +0300

    Movie-adapter-and-style

--- app/src/main/java/com/craftingsoftware/androidshow/ui/movies/Movies.kt
+++ app/src/main/java/com/craftingsoftware/androidshow/ui/movies/Movies.kt
@@ -4,14 +4,14 @@ import android.arch.lifecycle.Observer
 import android.support.v4.app.Fragment
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import com.craftingsoftware.androidshow.Adapter.MovieListAdapter
 import com.craftingsoftware.androidshow.R
 import com.craftingsoftware.androidshow.ui.home.Home
-import com.craftingsoftware.androidshow.ui.home.HomeViewModel
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_movies.*
 
 /**
  * Created by constantin.cheptea
@@ -27,15 +27,17 @@ class Movies : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MoviesViewModel::class.java)
+        recycle_movies.layoutManager = LinearLayoutManager(context)
 
         viewModel.getMovies().observe(this, Observer<List<Movie>> {
-            // new adapter with this list of movies
+
+            recycle_movies.adapter = MovieListAdapter(it)
         })
     }
 }
